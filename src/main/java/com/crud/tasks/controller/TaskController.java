2dc23fc011676177721d3f679ebadc08a176c9cd
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-03-25 21:48:11 +0100

    19.2 getTask method implementation

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/tasks")
@@ -25,7 +26,8 @@ public class TaskController {
 
     @GetMapping(value = "/{taskId}")
     public TaskDto getTask(@PathVariable("taskId") Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+        Task task = service.getTask(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
 
     @DeleteMapping
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +18,13 @@ public class DbService {
         return repository.findAll();
     }
 
+    public Task getTask(Long taskId) {
+        return repository.findById(taskId)
+                .map(task -> new Task(
+                        task.getId(),
+                        task.getTitle(),
+                        task.getContent())
+                )
+                .orElseThrow();
+    }
 }
