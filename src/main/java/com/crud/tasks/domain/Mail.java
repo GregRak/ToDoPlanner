db7dd5495bc7acf9e08e8558278001d38f86d167
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-06-04 18:26:14 +0200

    Adding possibility to send email to  CC

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,12 +1,26 @@
 package com.crud.tasks.domain;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@Builder
 public class Mail {
     private final String mailTo;
+    private String toCc;
     private final String subject;
     private final String message;
+
+//    public Mail(final String mailTo, final String subject, final String message) {
+//        this.mailTo = mailTo;
+//        this.subject = subject;
+//        this.message = message;
+//    }
+//
+//    public Mail(final String mailTo, final String toCc, final String subject, final String message) {
+//        this.mailTo = mailTo;
+//        this.toCc = toCc;
+//        this.subject = subject;
+//        this.message = message;
+//    }
 }
