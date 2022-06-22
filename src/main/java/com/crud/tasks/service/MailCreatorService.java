ea19c82be1ed98c206066333427d0b7e2ac80560
Grzegorz Rakowski <grzegorz.m.rakowski@gmail.com>
2022-06-22 22:46:07 +0200

    Thymeleaf: adding email template - update

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.AppInfoConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private AppInfoConfig appInfoConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -23,6 +27,10 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("app_name", appInfoConfig.getInfoAppName());
+        context.setVariable("company_name", appInfoConfig.getInfoCompanyName());
+        context.setVariable("company_email", appInfoConfig.getInfoCompanyEmail());
+        context.setVariable("company_phone", appInfoConfig.getInfoCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
